bd22a0f7ed9168c1e53e94a0103d2807566f3e19
poteii <poteii.pasel@icloud.com>
2017-08-15 19:03:30 +0700

    commit

---
+++ devBotDemo-1/src/main/java/com/demo/devbot/DevBotDemo1Application.java
@@ -0,0 +1,12 @@
+package com.demo.devbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevBotDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevBotDemo1Application.class, args);
+	}
+}
