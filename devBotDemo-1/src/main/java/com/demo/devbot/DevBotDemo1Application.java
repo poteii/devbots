37549408e5d922427b37cdad35676dd387330a3e
poteii <poteii.pasel@icloud.com>
2017-08-15 19:14:29 +0700

    java

--- devBotDemo-1/src/main/java/com/demo/devbot/DevBotDemo1Application.java
+++ devBotDemo-1/src/main/java/com/demo/devbot/DevBotDemo1Application.java
@@ -3,10 +3,30 @@ package com.demo.devbot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
 @SpringBootApplication
+//step 1 add @LineMessageHandle
+@LineMessageHandler
 public class DevBotDemo1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevBotDemo1Application.class, args);
 	}
+	
+	@EventMapping
+	public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+        System.out.println("event: " + event);
+        return new TextMessage(event.getMessage().getText());
+    }
+
+    @EventMapping
+    public void handleDefaultMessageEvent(Event event) {
+        System.out.println("event: " + event);
+    }
 }
