49381c99d751b3a06dabf9cd275ae73c070de5eb
poteii <poteii.pasel@icloud.com>
2017-08-15 19:40:44 +0700

    123

--- devBotDemo-1/src/main/java/com/demo/devbot/DevBotDemo1Application.java
+++ devBotDemo-1/src/main/java/com/demo/devbot/DevBotDemo1Application.java
@@ -2,6 +2,8 @@ package com.demo.devbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
@@ -14,6 +16,12 @@ import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 //step 1 add @LineMessageHandle
 @LineMessageHandler
 public class DevBotDemo1Application {
+	
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevBotDemo1Application.class, args);
